46a02d0d8867886c17ea18474f8c238c2af2e71f
PernilleSK <pernillesarahkaasing@gmail.dk>
2021-11-18 14:08:24 +0100

    Tilføjede state funktion

--- Main/Main.ino
+++ Main/Main.ino
@@ -2,22 +2,33 @@
 #include "MMA7660.h"
 #include <Wire.h>
 
-int tilstand;
+//int tilstand;
 rgb_lcd lcd;
-MMA7660 accelMeter;
+MMA7660 accelmeter;
 
 long randomElevR;
+
+
+// Variabler til functionGravity
+bool stadieFlag1 = 0;
+bool stadieFlag2 = 0;
+float x, y, z;
+float stadieGravity;
+long stadieCount = 1;
 
 void setup() 
 {
   lcd.begin(16, 2);
   lcd.setCursor(16, 2);
-  
+
+  //starter positionsmåledimsen
+  accelmeter.init();
+ 
 }
 
 void loop() 
 {
   //Temperatur();
-  RandomElev(); //Test
-  
+  //RandomElev(); //Test
+  functionGravity(4);
 }
--- Main/MainFunktioner.ino
+++ Main/MainFunktioner.ino
@@ -1,8 +1,65 @@
 
-/*int functionGravity(6)      //MNE
+
+
+int functionGravity(int stadie)
 {
-  
-}*/
+
+    stadieGravity = accelmeter.getAcceleration(&x, &y, &z); // Tager memory address som input
+    lcd.clear();
+    lcd.print(x);
+    lcd.print(" ");
+    lcd.print(y);
+    lcd.print(" ");
+    lcd.print(z);
+    delay(50);
+
+
+    if(x < 0.6)
+    {
+      stadieFlag1 = 0;
+    }
+    
+    if(x > 0.6 && stadieFlag1 == 0)
+    {
+      stadieCount = stadieCount + 1;
+      stadieFlag1 = 1;
+    }
+
+
+        if(x > -0.6)
+    {
+      stadieFlag2 = 0;
+    }
+    
+    if(x < -0.6 && stadieFlag2 == 0)
+    {
+      stadieCount = stadieCount - 1;
+      stadieFlag2 = 1;
+    }
+
+
+      //flag sørger for at vi skal give slip på knappen før der kan lægges 1 til count igen
+     lcd.setCursor(0, 1);
+   lcd.print(" " );
+      lcd.print(stadieCount);
+     delay(100);
+
+
+    if(stadieCount < 1)
+    {
+      stadieCount = stadie;
+    }
+
+    if(stadieCount > stadie)
+    {
+      stadieCount = 1;
+    }
+
+    return stadieCount;
+}
+
+
+
 
 void Ur()            //MNE
 {
