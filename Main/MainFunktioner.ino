7eaec204f83c1648af10858802c7a754305c82fe
PernilleSK <pernillesarahkaasing@gmail.dk>
2021-12-07 11:47:05 +0100

    Stadiecount virker hehe

--- Main/MainFunktioner.ino
+++ Main/MainFunktioner.ino
@@ -5,13 +5,13 @@ int functionGravity(int stadie)
 {
 
     stadieGravity = accelmeter.getAcceleration(&x, &y, &z); // Tager memory address som input
-    lcd.clear();
+    /*lcd.clear();
     lcd.print(x);
     lcd.print(" ");
     lcd.print(y);
     lcd.print(" ");
     lcd.print(z);
-    delay(50);
+    delay(50);*/
 
 
     if(x < 0.6)
@@ -40,8 +40,6 @@ int functionGravity(int stadie)
 
       //flag sørger for at vi skal give slip på knappen før der kan lægges 1 til count igen
      lcd.setCursor(0, 1);
-   lcd.print(" " );
-      lcd.print(stadieCount);
      delay(100);
 
 
@@ -250,5 +248,64 @@ void RandomElev()    //PSK
 
 void Spil()         //??
 {
+  lcd.clear();
+
+  knap = digitalRead(4);
+        
+  // set the cursor to column 0, line 1
   
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 0);
+  
+  spilValue = analogRead(A0);
+
+  lcd.print(spilValue);
+  //delay(100);
+
+   //int randomNummer = random (50,973);
+   lcd.setCursor(0, 1);
+   lcd.print(randomNummer);
+   //delay(100);
+
+
+   if(spilValue > randomNummer -50 && spilValue < randomNummer + 50)
+   {
+        lcd.setRGB(245,212,66);
+  
+        FunctionSos();
+   }
+  
+   if(spilValue > randomNummer + 50 or spilValue < randomNummer - 50)
+   {
+    lcd.setRGB(255, 255, 255);
+   }
+  
+   if(spilValue == 0)
+   {
+    randomNummer = random (50,973);
+    lcd.setRGB(255, 255, 255);
+   }
 }
+}
+
+void FunctionSos()
+{
+
+   if(spilValue > randomNummer - 25 && spilValue < randomNummer + 25)
+   {
+      if(knap == 1)
+      {
+        lcd.setRGB(0, 255, 10); //aka rigtigt
+        delay(5);
+      }
+   }
+  
+  
+   if(spilValue > randomNummer + 25 || spilValue < randomNummer - 25)
+   {
+      if(knap == 1)
+      {
+        lcd.setRGB(255, 0, 0); //aka forkert
+      }
+   }
+ }
---
+++ Main/UnderFunktioner.ino
@@ -0,0 +1,21 @@
+/*void functionSos()
+{
+
+   if(spilValue > randomNummer - 25 && spilValue < randomNummer + 25)
+   {
+      if(knap == 1)
+      {
+        lcd.setRGB(0, 255, 10); //aka rigtigt
+        delay(5);
+      }
+   }
+  
+  
+   if(spilValue > randomNummer + 25 || spilValue < randomNummer - 25)
+   {
+      if(knap == 1)
+      {
+        lcd.setRGB(255, 0, 0); //aka forkert
+      }
+   }
+ }*/
